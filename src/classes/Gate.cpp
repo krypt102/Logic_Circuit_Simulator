cad1abf7f69230c33a9cbc2be552756c84141982
krypt102 <kryptonforce102@gmail.com>
2026-05-15 13:55:13 +1000

    Add gate class in preparation for circuit creation

---
+++ src/classes/Gate.cpp
@@ -0,0 +1,26 @@
+#include "Gate.hpp"
+
+std::string gate_type_to_string(GateType type) {
+    switch (type) {
+        case GateType::AND:
+            return "AND";
+        case GateType::OR:
+            return "OR";
+        case GateType::NOT:
+            return "NOT";
+        case GateType::NAND:
+            return "NAND";
+        case GateType::NOR:
+            return "NOR";
+        case GateType::XOR:
+            return "XOR";
+    }
+    return "UNKNOWN";
+}
+
+int input_count_for_type(GateType type) {
+    if (type == GateType::NOT) {
+        return 1;
+    };
+    return 2;
+}
---
+++ src/classes/Gate.hpp
@@ -0,0 +1,44 @@
+#ifndef LOGIC_CIRCUIT_SIMULATOR_GATE_HPP
+#define LOGIC_CIRCUIT_SIMULATOR_GATE_HPP
+
+#include <string>
+#include <vector>
+#include "Pin.hpp"
+
+enum class GateType {
+    AND,
+    OR,
+    NOT,
+    NAND,
+    NOR,
+    XOR
+};
+
+std::string gate_type_to_string(GateType type);
+
+int input_count_for_type(GateType type);
+
+struct Gate {
+    int id;
+    float x_position;
+    float y_position;
+
+    GateType gate_type;
+    std::vector<Pin> input_pins;
+    std::vector<Pin> output_pins;
+
+    Gate(int id, GateType type, float x, float y)
+        : id(id),
+          x_position(x),
+          y_position(y),
+          gate_type(type)
+    {
+        int input_count = input_count_for_type(type);
+        for (int i = 0; i < input_count; i++) {
+            input_pins.emplace_back(i, 0.0f, 0.0f, false);
+        }
+        output_pins.emplace_back(input_count, 0.0f, 0.0f, true);
+    }
+};
+
+#endif //LOGIC_CIRCUIT_SIMULATOR_GATE_HPP
