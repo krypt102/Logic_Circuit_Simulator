5f4fa44d1e139be1574f16efba2de78a9cccd1b0
krypt102 <kryptonforce102@gmail.com>
2026-06-10 16:10:15 +1000

    Update version strings again

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ int main() {
             break;
         }
         draw_interface();
-        draw_text(std::string(APP_VERSION), COLOR_GRAY, "JetBrainsMono-Regular", 16, 10.0f, window_handler.window_height - 30.0f);
+        draw_text(std::format("LCS-{}", std::string(APP_VERSION)), COLOR_GRAY, "JetBrainsMono-Regular", 16, 10.0f, window_handler.window_height - 30.0f);
 
         refresh_screen(60);
     }
